19e473c85c9586208441efa8f6c8a9755abb96db
Andres-J-Correa <andresj.correas@gmail.com>
2025-05-22 19:54:37 +0800

    done

--- client/src/components/bookings/BookingView.jsx
+++ client/src/components/bookings/BookingView.jsx
@@ -15,7 +15,18 @@ import LoadingOverlay from "components/commonUI/loaders/LoadingOverlay";
 import Breadcrumb from "components/commonUI/Breadcrumb";
 import ErrorBoundary from "components/commonUI/ErrorBoundary";
 import Swal from "sweetalert2";
-import { Button } from "reactstrap";
+import dayjs from "dayjs";
+import isSameOrBefore from "dayjs/plugin/isSameOrBefore";
+import { Button, Col, Row } from "reactstrap";
+import {
+  faCalendarCheck,
+  faRectangleXmark,
+  faPlaneCircleCheck,
+  faPlaneCircleXmark,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+dayjs.extend(isSameOrBefore);
 
 function BookingView() {
   const { hotelId, bookingId } = useParams();
@@ -92,7 +103,7 @@ function BookingView() {
       if (res.isSuccessful) {
         setBooking((prevBooking) => ({
           ...prevBooking,
-          status: BOOKING_STATUS_DICTIONARY.ARRIVED,
+          status: { id: BOOKING_STATUS_DICTIONARY.ARRIVED },
         }));
 
         Swal.fire({
@@ -114,6 +125,162 @@ function BookingView() {
     }
   }, [hasBalanceDue, bookingId]);
 
+  const handleNoShow = useCallback(async () => {
+    const result = await Swal.fire({
+      title: "¿Desea marcar la reserva como no show?",
+      text: "Ya no podrá editar la reserva",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonText: "Sí, confirmar",
+      cancelButtonText: "Cancelar",
+      reverseButtons: true,
+      confirmButtonColor: "red",
+    });
+
+    if (!result.isConfirmed) {
+      return;
+    }
+
+    try {
+      Swal.fire({
+        title: "Cargando...",
+        text: "Por favor, espere.",
+        allowOutsideClick: false,
+        didOpen: () => {
+          Swal.showLoading();
+        },
+      });
+
+      const res = await updateStatusToNoShow(bookingId);
+      if (res.isSuccessful) {
+        setBooking((prevBooking) => ({
+          ...prevBooking,
+          status: { id: BOOKING_STATUS_DICTIONARY.NO_SHOW },
+        }));
+
+        Swal.fire({
+          title: "Éxito",
+          text: "Reserva marcada como no show",
+          icon: "success",
+          confirmButtonText: "Aceptar",
+        });
+      } else {
+        throw new Error("Error al marcar como no show");
+      }
+    } catch (error) {
+      Swal.close();
+      await Swal.fire({
+        icon: "error",
+        title: "Error",
+        text: "Error al marcar como no show",
+      });
+    }
+  }, [bookingId]);
+
+  const handleComplete = useCallback(async () => {
+    const result = await Swal.fire({
+      title: "¿Desea marcar la reserva como completada?",
+      text: "Ya no podrá editar la reserva",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonText: "Sí, confirmar",
+      cancelButtonText: "Cancelar",
+      reverseButtons: true,
+      confirmButtonColor: "green",
+    });
+
+    if (!result.isConfirmed) {
+      return;
+    }
+
+    try {
+      Swal.fire({
+        title: "Cargando...",
+        text: "Por favor, espere.",
+        allowOutsideClick: false,
+        didOpen: () => {
+          Swal.showLoading();
+        },
+      });
+
+      const res = await updateStatusToCompleted(bookingId);
+      if (res.isSuccessful) {
+        setBooking((prevBooking) => ({
+          ...prevBooking,
+          status: { id: BOOKING_STATUS_DICTIONARY.COMPLETED },
+        }));
+
+        Swal.fire({
+          title: "Éxito",
+          text: "Reserva marcada como completada",
+          icon: "success",
+          confirmButtonText: "Aceptar",
+        });
+      } else {
+        throw new Error("Error al marcar como completada");
+      }
+    } catch (error) {
+      Swal.close();
+      await Swal.fire({
+        icon: "error",
+        title: "Error",
+        text: "Error al marcar como completada",
+      });
+    }
+  }, [bookingId]);
+
+  const handleCancel = useCallback(async () => {
+    const result = await Swal.fire({
+      title: "¿Desea cancelar la reserva?",
+      text: "Ya no podrá editar la reserva",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonText: "Sí, confirmar",
+      cancelButtonText: "Cancelar",
+      reverseButtons: true,
+      confirmButtonColor: "red",
+    });
+
+    if (!result.isConfirmed) {
+      return;
+    }
+
+    try {
+      Swal.fire({
+        title: "Cargando...",
+        text: "Por favor, espere.",
+        allowOutsideClick: false,
+        didOpen: () => {
+          Swal.showLoading();
+        },
+      });
+
+      const res = await updateStatusToCancelled(bookingId);
+      if (res.isSuccessful) {
+        setBooking((prevBooking) => ({
+          ...prevBooking,
+          status: { id: BOOKING_STATUS_DICTIONARY.CANCELLED },
+        }));
+
+        Swal.fire({
+          title: "Éxito",
+          text: "Asegúrate de devolver depositos si corresponde",
+          icon: "info",
+          confirmButtonText: "Aceptar",
+        });
+      } else {
+        throw new Error("Error al cancelar la reserva");
+      }
+    } catch (error) {
+      Swal.close();
+      await Swal.fire({
+        icon: "error",
+        title: "Error",
+        text: "Error al cancelar la reserva",
+      });
+    }
+  }, [bookingId]);
+
   useEffect(() => {
     if (bookingId) {
       setIsLoading(true);
@@ -128,19 +295,54 @@ function BookingView() {
     <>
       <Breadcrumb breadcrumbs={breadcrumbs} active="Reserva" />
       <h3 className="mb-4">Reserva</h3>
-      <div className="mb-3">
-        {booking?.status?.id === BOOKING_STATUS_DICTIONARY.ACTIVE && (
-          <Button
-            color="outline-success"
-            className="ms-2"
-            onClick={handleCheckIn}>
-            Check-in
-          </Button>
-        )}
-        <Link to="edit" className="btn btn-outline-dark float-end">
-          Editar
-        </Link>
-      </div>
+      <Row className="mb-3">
+        <Col className="px-3">
+          {booking?.status?.id === BOOKING_STATUS_DICTIONARY.ACTIVE && (
+            <Button color="outline-dark" onClick={handleCheckIn}>
+              Marcar Check-in
+              <FontAwesomeIcon icon={faCalendarCheck} className="ms-2" />
+            </Button>
+          )}
+          {(booking?.status?.id === BOOKING_STATUS_DICTIONARY.ACTIVE ||
+            booking?.status?.id === BOOKING_STATUS_DICTIONARY.ARRIVED) &&
+            dayjs(booking?.departureDate).isSameOrBefore(dayjs()) && (
+              <Button
+                color="outline-dark"
+                className="ms-2"
+                onClick={handleComplete}>
+                Marcar Completada
+                <FontAwesomeIcon icon={faPlaneCircleCheck} className="ms-2" />
+              </Button>
+            )}
+          {booking?.status?.id === BOOKING_STATUS_DICTIONARY.ACTIVE &&
+            dayjs(booking?.arrivalDate).isSameOrBefore(dayjs()) && (
+              <Button
+                color="outline-dark"
+                className="ms-2"
+                onClick={handleNoShow}>
+                Marcar No Show
+                <FontAwesomeIcon icon={faPlaneCircleXmark} className="ms-2" />
+              </Button>
+            )}
+          {booking?.status?.id !== BOOKING_STATUS_DICTIONARY.NO_SHOW &&
+            booking?.status?.id !== BOOKING_STATUS_DICTIONARY.COMPLETED &&
+            booking?.status?.id !== BOOKING_STATUS_DICTIONARY.CANCELLED && (
+              <Button
+                color="outline-danger"
+                className="ms-2 float-end"
+                onClick={handleCancel}>
+                Cancelar Reserva
+                <FontAwesomeIcon icon={faRectangleXmark} className="ms-2" />
+              </Button>
+            )}
+          {(booking?.status?.id === BOOKING_STATUS_DICTIONARY.ACTIVE ||
+            booking?.status?.id === BOOKING_STATUS_DICTIONARY.ARRIVED) && (
+            <Link to="edit" className="btn btn-outline-dark float-end">
+              Editar
+            </Link>
+          )}
+        </Col>
+      </Row>
       <LoadingOverlay isVisible={isLoading || isLoadingBookingData} />
       <ErrorBoundary>
         {booking !== null && (
