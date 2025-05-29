1bb18dc347eecc5a2ac7debb91a2026dfc95e058
Andres-J-Correa <andresj.correas@gmail.com>
2025-05-29 17:31:12 +0800

    saving

--- client/src/components/hotels/HotelsView.jsx
+++ client/src/components/hotels/HotelsView.jsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useState, useCallback } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 import { getAll } from "services/hotelService";
 import { Row, Col, Card } from "reactstrap";
 import LoadingOverlay from "components/commonUI/loaders/LoadingOverlay";
+import ErrorBoundary from "components/commonUI/ErrorBoundary";
 import Breadcrumb from "components/commonUI/Breadcrumb";
 import { toast } from "react-toastify";
 import "./hotelsview.css";
@@ -42,28 +43,29 @@ const HotelsView = () => {
     <>
       <Breadcrumb breadcrumbs={breadcrumbs} active="Hoteles" />
       <LoadingOverlay isVisible={isLoading} message="Cargando..." />
-      <Row>
-        {hotels.length > 0 ? (
-          hotels.map((hotel) => (
-            <Col key={hotel.id} xs="12" sm="6" md="4" lg="3" className="mb-3">
-              <Card
-                className="p-3 text-center hotel-card shadow bg-dark text-white"
-                onClick={() => handleCardClick(hotel.id)}
-                role="button">
-                {hotel.name}
-              </Card>
+      <ErrorBoundary>
+        <Row>
+          {hotels.length > 0 ? (
+            hotels.map((hotel) => (
+              <Col key={hotel.id} xs="12" sm="6" md="4" lg="3" className="mb-3">
+                <Card
+                  className="p-3 text-center hotel-card shadow bg-dark text-white"
+                  onClick={() => handleCardClick(hotel.id)}
+                  role="button">
+                  {hotel.name}
+                </Card>
+              </Col>
+            ))
+          ) : (
+            <Col xs="12" className="text-center">
+              <h5>No hay hoteles disponibles</h5>
+              <Link to="/hotels/add" className="btn btn-dark mt-3">
+                Agregar Hotel
+              </Link>
             </Col>
-          ))
-        ) : (
-          <Col xs="12" className="text-center">
-            <h5>No hay hoteles disponibles</h5>
-            <p>
-              Ve a la sección de "Alojamientos" en el menú superior para ver tus
-              invitaciones pendientes o agregar nuevos hoteles.
-            </p>
-          </Col>
-        )}
-      </Row>
+          )}
+        </Row>
+      </ErrorBoundary>
     </>
   );
 };
