2074c7ec69099dcc5629ee21f939207f6013a03f
Andres-J-Correa <andresj.correas@gmail.com>
2025-06-07 17:36:29 +0800

    changed transactions status badge to icons with titles

--- client/src/components/transactions/TransactionStatusBadge.jsx
+++ client/src/components/transactions/TransactionStatusBadge.jsx
@@ -1,13 +1,28 @@
 import React from "react";
 import {
   TRANSACTION_STATUS_IDS,
-  TRANSACTION_STATUSES,
+  TRANSACTION_STATUS_BY_ID,
 } from "components/transactions/constants";
 import classNames from "classnames";
+import {
+  faHourglass,
+  faCheckCircle,
+  faTimesCircle,
+  faArrowRotateLeft,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+const statusIcons = {
+  [TRANSACTION_STATUS_IDS.PENDING]: faHourglass,
+  [TRANSACTION_STATUS_IDS.COMPLETED]: faCheckCircle,
+  [TRANSACTION_STATUS_IDS.FAILED]: faTimesCircle,
+  [TRANSACTION_STATUS_IDS.REVERSED]: faArrowRotateLeft,
+};
 
 function TransactionStatusBadge({ statusId, className, ...props }) {
   return (
     <span
+      title={TRANSACTION_STATUS_BY_ID[statusId] || "Estado desconocido"}
       className={classNames("badge text-capitalize", className, {
         "bg-warning": statusId === TRANSACTION_STATUS_IDS.PENDING,
         "bg-success": statusId === TRANSACTION_STATUS_IDS.COMPLETED,
@@ -15,9 +30,7 @@ function TransactionStatusBadge({ statusId, className, ...props }) {
         "bg-secondary": statusId === TRANSACTION_STATUS_IDS.REVERSED,
       })}
       {...props}>
-      {TRANSACTION_STATUSES.find(
-        (status) => Number(status.id) === Number(statusId)
-      )?.name || "No definido"}
+      <FontAwesomeIcon icon={statusIcons[statusId] || faHourglass} />
     </span>
   );
 }
