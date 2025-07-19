70c5cde3c4988a4e2313e05cd21ac8fc3eabf62e
Andres-J-Correa <andresj.correas@gmail.com>
2025-07-19 17:37:27 +0800

    removing debugger

--- client/src/components/bookings/calendarV2/hooks/useCalendarTableData.ts
+++ client/src/components/bookings/calendarV2/hooks/useCalendarTableData.ts
@@ -92,7 +92,6 @@ export const useCalendarTableData = (
       if (response.isSuccessful) {
         setRoomAvailability(response.items);
       } else {
-        debugger;
         if (response.error?.response?.status !== 404) {
           toast.error(t("booking.calendar.errors.loadAvailability"));
         }
