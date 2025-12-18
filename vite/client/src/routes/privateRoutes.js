23b55f138e6a751ed0ab15de26ded60a947855e6
Andres-J-Correa <andresj.correas@gmail.com>
2025-12-18 16:54:24 +0800

    route components/bookings/booking-quote-view/BookingQuoteView done

--- vite/client/src/routes/privateRoutes.js
+++ vite/client/src/routes/privateRoutes.js
@@ -106,13 +106,13 @@ const bookings = [
       import("components/bookings/booking-add-edit-view-v2/BookingAddEditView")
     ),
   },
-  //   {
-  //     name: "quoteBooking",
-  //     path: "/hotels/:hotelId/bookings/quote",
-  //     component: lazy(() =>
-  //       import("components/bookings/booking-quote-view/BookingQuoteView")
-  //     ),
-  //   },
+  {
+    name: "quoteBooking",
+    path: "/hotels/:hotelId/bookings/quote",
+    component: lazy(() =>
+      import("components/bookings/booking-quote-view/BookingQuoteView")
+    ),
+  },
   //   {
   //     name: "viewBooking",
   //     path: "/hotels/:hotelId/bookings/:bookingId",
