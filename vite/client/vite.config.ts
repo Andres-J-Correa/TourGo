32c000bcb57f9afc9f16494063654c606bdd5b80
Andres-J-Correa <andresj.correas@gmail.com>
2025-12-19 10:19:55 +0800

    adding config for installing on devices

--- vite/client/vite.config.ts
+++ vite/client/vite.config.ts
@@ -1,13 +1,57 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import commonjs from "vite-plugin-commonjs";
+import { VitePWA } from "vite-plugin-pwa";
 
 import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   base: "/app/",
-  plugins: [react(), commonjs()],
+  plugins: [
+    react(),
+    commonjs(),
+    VitePWA({
+      registerType: "autoUpdate",
+      base: "/app/",
+      manifest: {
+        short_name: "TourGo",
+        name: "TourGo",
+        description:
+          "Administra tu hotel, gestiona reservas y ofrece una experiencia única a tus huéspedes con TourGo.",
+        scope: "/app/",
+        start_url: "/app/",
+        orientation: "portrait",
+        display: "standalone",
+        theme_color: "#344767",
+        background_color: "#ffffff",
+        icons: [
+          {
+            src: "/app/favicon.ico",
+            sizes: "64x64 32x32 24x24 16x16",
+            type: "image/x-icon",
+          },
+          {
+            src: "/app/logo192.png",
+            type: "image/png",
+            sizes: "192x192",
+            purpose: "any",
+          },
+          {
+            src: "/app/logo512.png",
+            type: "image/png",
+            sizes: "512x512",
+            purpose: "any",
+          },
+          {
+            src: "/app/apple-touch-icon.png",
+            type: "image/png",
+            sizes: "180x180",
+          },
+        ],
+      },
+    }),
+  ],
   server: {
     proxy: {
       "/api": {
