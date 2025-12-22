c537453c8d171806dabd1f6a41495a6482a0d6c7
Andres-J-Correa <andresj.correas@gmail.com>
2025-12-22 13:15:55 +0800

    updating protected routes to prevent redirect on initial render

--- client/src/routes/ProtectedRoutes.tsx
+++ client/src/routes/ProtectedRoutes.tsx
@@ -4,7 +4,7 @@ import { useAppContext } from "contexts/GlobalAppContext";
 const ProtectedRoutes = () => {
   const { user } = useAppContext();
 
-  if (!user.current.isAuthenticated) {
+  if (!user.current.isAuthenticated && !user.isLoading) {
     return <Navigate to="/" replace />;
   }
 
